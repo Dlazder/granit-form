2fb9a5015dd702a3dd0bf05847991cc5b538e7ef
dlazder <artist.aksenov@yandex.ru>
2023-07-22 19:45:53 +0300

    fix images not upload bug

--- script.js
+++ script.js
@@ -197,7 +197,7 @@ function uploadFile(e) {
                 div.dataset.image = fileName;
                 div.innerHTML = `
                 <img src="${reader.result}"></img>
-                <span class="delete-btn"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 16 16" class=""><path fill-rule="evenodd" d="M6.586 8L4.293 5.707a.999.999 0 111.414-1.414L8 6.586l2.293-2.293a1 1 0 011.414 1.414L9.414 8l2.293 2.293a1 1 0 01-1.414 1.414L8 9.414l-2.293 2.293a1 1 0 01-1.414-1.414L6.586 8z"></path></svg></span>
+                <span class="delete-btn"><svg width="30" height="30" viewBox="0 0 16 16" class=""><path fill-rule="evenodd" d="M6.586 8L4.293 5.707a.999.999 0 111.414-1.414L8 6.586l2.293-2.293a1 1 0 011.414 1.414L9.414 8l2.293 2.293a1 1 0 01-1.414 1.414L8 9.414l-2.293 2.293a1 1 0 01-1.414-1.414L6.586 8z"></path></svg></span>
                 `;
                 div.querySelector('.delete-btn').addEventListener('click', () => {
                     fetch('/delete-file', {method: 'POST', body: div.dataset.image})
